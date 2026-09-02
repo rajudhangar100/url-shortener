8bdcf71f87c69b5a14980ec0f9a2e1be366aa6a4
rajudhangar100 <rajudhangartayappa.gmail.com>
2026-09-02 19:55:40 +0530

    removed nginx

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -5,10 +5,10 @@ export default function Homepage() {
   return (
     <>
           <Heading as="h3" size="xl" m={ '3% 0% 2% 0%' }>
-        URL Shortener
+        SwiftLink
           </Heading>
           <InputForm />
-          <Text m={ '3%' }>Made with ❤️ by Me ( <a target="_blank" href="https://akshay-kumar-portfoilo.netlify.app/"> Akshay Kumar</a>)</Text>
+          <Text m={ '3%' }>Made with ❤️ by Me ( <a target="_blank" href="https://github.com/rajudhangar100"> Raju Dhangar</a>)</Text>
     </>
   )
 }
